d0f144bdc5613b0845f6c9d2b7e361cbc4baf27a
James Reed <jcrd@tuta.io>
2021-02-23 16:43:10 -0700

    Fix clock x positioning

--- wio-pomodoro.ino
+++ wio-pomodoro.ino
@@ -118,7 +118,7 @@ void draw_battery_bar() {
 void update(int update_image) {
     int image_x = SCREEN_W / 2 - CLOCK_LEN / 2 - IMAGE_SIZE / 2;
     int image_y = SCREEN_H / 2 - IMAGE_SIZE / 2;
-    int clock_x = image_x + IMAGE_SIZE / 2 + FONT_SIZE;
+    int clock_x = image_x + IMAGE_SIZE;
     int clock_y = SCREEN_H / 2 - FONT_SIZE / 2;
     int rep_x = clock_x + CLOCK_LEN - FONT_SIZE / 2;
     int rep_y = SCREEN_H / 2 - FONT_SIZE / 2 - 4;
