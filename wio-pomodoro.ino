d28fe4066222cc397f0ec09a3ae6c2c3cbec0294
James Reed <jcrd@tuta.io>
2021-02-28 16:29:43 -0700

    Make clock blink when paused

--- wio-pomodoro.ino
+++ wio-pomodoro.ino
@@ -59,9 +59,12 @@ const int REP_BAR_H = 8;
 const int REP_BAR_DIV = 2;
 
 const unsigned long UPDATE_MS = 1000;
+const unsigned long BLINK_MS = 400;
 const unsigned long DEBOUNCE_MS = 200;
 
 unsigned long last_update = 0;
+unsigned long last_blink = 0;
+int blinking = 0;
 int running = 0;
 int state = STOPPED;
 int countdown = 0;
@@ -94,14 +97,6 @@ void inc_brightness(int i) {
     backlight.setBrightness(brightness);
 }
 
-void draw_paused(int x, int y) {
-    uint32_t color = TFT_BLACK;
-    if (!running && state != STOPPED)
-        color = TFT_WHITE;
-    tft.fillRect(x, y, PAUSED_W, PAUSED_H, color);
-    tft.fillRect(x + PAUSED_W * 2, y, PAUSED_W, PAUSED_H, color);
-}
-
 void draw_battery_bar() {
     int soc = lipo.soc();
     if (soc == last_battery_soc)
@@ -127,7 +122,11 @@ void draw_rep_bar(int x, int y, int w) {
         tft.fillRect(x + i * rep_div, y, rep_w, REP_BAR_H, TFT_WHITE);
 }
 
-void update(int state_changed) {
+void erase_clock(int x, int y) {
+    tft.fillRect(x, y, CLOCK_LEN, FONT_SIZE + 2, TFT_BLACK);
+}
+
+void update(int state_changed, int blink) {
     int image_x = SCREEN_W / 2 - CLOCK_LEN / 2 - IMAGE_SIZE / 2;
     int image_y = SCREEN_H / 2 - IMAGE_SIZE / 2;
     int clock_x = image_x + IMAGE_SIZE + FONT_SIZE / 2;
@@ -139,17 +138,23 @@ void update(int state_changed) {
     int m = countdown / 60;
     int s = countdown % 60;
 
+    if (blink && !blinking) {
+        erase_clock(clock_x, clock_y);
+        blinking = 1;
+        return;
+    } else {
+        blinking = 0;
+    }
+
     if (state_changed) {
         tft.fillRect(image_x, image_y, IMAGE_SIZE, IMAGE_SIZE, TFT_BLACK);
         draw_rep_bar(image_x, image_y + IMAGE_SIZE, IMAGE_SIZE + CLOCK_LEN);
     }
     images[state]->draw(image_x, image_y);
 
-    tft.fillRect(clock_x, clock_y, CLOCK_LEN, FONT_SIZE + 2, TFT_BLACK);
+    erase_clock(clock_x, clock_y);
     snprintf(clock_buf, sizeof(clock_buf), "%02d:%02d", m, s);
     tft.drawString(clock_buf, clock_x, clock_y);
-
-    draw_paused(paused_x, paused_y);
 
     if (has_battery)
         draw_battery_bar();
@@ -184,50 +189,11 @@ void setup() {
     tft.setRotation(3);
     tft.fillScreen(TFT_BLACK);
     tft.setFreeFont(LARGE_FONT);
-    update(0);
+    update(0, 0);
 }
 
-void loop() {
-    int last_state = state;
-
-    for (int i = 0; i < INPUT_N; i++) {
-        if (digitalRead(input_map[i]) == LOW && input_state[i] != LOW) {
-            input_state[i] = LOW;
-            last_input[i] = millis();
-        }
-        if (input_state[i] != LOW || millis() - last_input[i] < DEBOUNCE_MS)
-            continue;
-        switch (i) {
-            case RESET:
-                running = 0;
-                rep = 0;
-                state = STOPPED;
-                countdown = 0;
-                update(1);
-                break;
-            case TOGGLE_RUNNING:
-                if (state == STOPPED) {
-                    running = 1;
-                    rep = 1;
-                    state = WORKING;
-                    countdown = WORKING_SEC;
-                    update(1);
-                } else {
-                    running = !running;
-                    update(0);
-                }
-                break;
-            case BACKLIGHT_UP:
-                inc_brightness(BRIGHTNESS_STEP);
-                break;
-            case BACKLIGHT_DOWN:
-                inc_brightness(-BRIGHTNESS_STEP);
-                break;
-        }
-        input_state[i] = HIGH;
-    }
-
-    if (running && millis() - last_update >= UPDATE_MS) {
+void run(int last_state) {
+    if (millis() - last_update >= UPDATE_MS) {
         if (--countdown == 0) {
             switch (state) {
                 case WORKING:
@@ -253,6 +219,55 @@ void loop() {
             }
         }
 
-        update(state != last_state);
+        update(state != last_state, 0);
+    }
+}
+
+void loop() {
+    int last_state = state;
+
+    for (int i = 0; i < INPUT_N; i++) {
+        if (digitalRead(input_map[i]) == LOW && input_state[i] != LOW) {
+            input_state[i] = LOW;
+            last_input[i] = millis();
+        }
+        if (input_state[i] != LOW || millis() - last_input[i] < DEBOUNCE_MS)
+            continue;
+        switch (i) {
+            case RESET:
+                running = 0;
+                rep = 0;
+                state = STOPPED;
+                countdown = 0;
+                update(1, 0);
+                break;
+            case TOGGLE_RUNNING:
+                if (state == STOPPED) {
+                    running = 1;
+                    rep = 1;
+                    state = WORKING;
+                    countdown = WORKING_SEC;
+                    update(1, 0);
+                } else {
+                    running = !running;
+                    update(0, 0);
+                }
+                break;
+            case BACKLIGHT_UP:
+                inc_brightness(BRIGHTNESS_STEP);
+                break;
+            case BACKLIGHT_DOWN:
+                inc_brightness(-BRIGHTNESS_STEP);
+                break;
+        }
+        input_state[i] = HIGH;
+    }
+
+
+    if (running) {
+        run(state);
+    } else if (state != STOPPED && millis() - last_blink >= BLINK_MS) {
+        update(0, 1);
+        last_blink = millis();
     }
 }
